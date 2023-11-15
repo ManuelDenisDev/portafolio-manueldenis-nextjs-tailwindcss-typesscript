57595b4516b8a479f52cb80182cc01e6752e8424
Manuel Denis Developer <manueldenis.developer@gmail.com>
2023-11-15 08:15:37 +0100

    correcion imagenes

--- components/iu/ArticleCardServicios.tsx
+++ components/iu/ArticleCardServicios.tsx
@@ -13,21 +13,18 @@ const ArticleCardServicios = ({ className }: ArticleCardServiciosProps) => {
                 imagen="https://framerusercontent.com/images/asSnXTpbloEzEwWxiy8QxOVsJxM.svg"
                 titulo="Landing Page"
                 descripcion="La meta principal de una landing page es la conversión, es decir, buscamos que los visitantes realicen acciones específicas, como suscribirse a una lista de correo, solicitar un presupuesto, agendar una cita gratuita o descargar una guía."
-                precio="299€ + IVA"
             />
             <CardServicios
                 href="/"
                 imagen="https://framerusercontent.com/images/cP1ggFAijMP3KbQH9lgJ1E1iY.svg"
                 titulo="Web Basic"
                 descripcion="Es la mejor opción si necesitas una presencia online estéticamente impecable y un plan SEO básico para empezar."
-                precio="499€ + IVA"
             />
             <CardServicios
                 href="/"
                 imagen="https://framerusercontent.com/images/j1lb2OAfQQJC6MTGWnIwnAXtaM.svg"
                 titulo="Web Premium"
                 descripcion="Esta opción es la elección ideal si buscas una presencia online que no solo sea visualmente atractiva, sino que también ofrezca una amplia gama de características adicionales (reservas, calendario de eventos, varios idiomas, boletines de noticias...)."
-                precio="799€ + IVA"
             />
         </article>
     )
